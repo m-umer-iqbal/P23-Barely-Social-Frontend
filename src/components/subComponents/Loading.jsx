fb9e8f4054fa4eac42a9a57b77a71a8c6fb8c849
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-11-16 23:33:08 +0500

    Add Line in Loading

--- src/components/subComponents/Loading.jsx
+++ src/components/subComponents/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-dark-blue-900">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-dark-blue-900 gap-8">
             <div className="relative w-32 h-32">
                 <div
                     className="absolute w-full h-full rounded-full border-8 border-gray-100/10 border-r-blue-400 border-b-blue-400 animate-spin"
@@ -16,6 +16,9 @@ const Loading = () => {
                     className="absolute inset-0 bg-gradient-to-top-right from-blue-400/10 via-transparent to-blue-400/5 animate-pulse rounded-full blur-sm"
                 ></div>
             </div>
+            <p className="text-blue-400 text-4xl animate-pulse">
+                Loading… Because Being Social Takes Efforts...
+            </p>
         </div>
     )
 }
