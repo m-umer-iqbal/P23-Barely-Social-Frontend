b13854ac80ec9cf50f75d564314fc8d62e0cdde5
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-11-25 05:15:36 +0500

    Add reset functionality to form submission in LeftSideBar component and Corrected the display of logout button and added functionality of logout button

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -12,6 +12,7 @@ const LeftSideBar = (props) => {
         handleSubmit,
         setValue,
         watch,
+        reset,
         formState: { errors, isSubmitting, isDirty },
     } = useForm({
         defaultValues: {
@@ -49,6 +50,7 @@ const LeftSideBar = (props) => {
             let data = await response.json();
             if (data.success) {
                 alert(data.message)
+                reset(formdata);
                 setShowUpdateBtn(false); // Hide after successful update
                 navigate("/home")
             } else {
@@ -95,149 +97,151 @@ const LeftSideBar = (props) => {
     }, [isDirty]);
 
     return (
-        <form className='flex flex-col items-center gap-8 min-w-[25vw] max-w-[25vw] text-4xl p-8 bg-off-blue-200 text-dark-blue-900 rounded-4xl' onSubmit={handleSubmit(onSubmit)}>
-            <div className='flex flex-col justify-center items-center gap-2'>
-                <img
-                    src="https://picsum.photos/2000.webp"
-                    alt="Profile-Picture"
-                    className='w-60 rounded-[100%] border-8 border-dark-blue-900'
-                />
-                <div className='flex justify-center items-baseline space-x-4 relative group'
-                    onMouseOver={() => { setNameEdit(true) }}
-                    onMouseOut={() => { setNameEdit(false) }}>
-                    <input
-                        type="text"
-                        value={fullnameValue}
-                        autoComplete='off'
-                        placeholder='Full Name'
-                        className='font-semibold text-center field-sizing-content focus:outline-none focus:border-b-4 focus:border-dark-blue-900 border-transparent bg-transparent text-dark-blue-900 placeholder-mid-blue-700'
-                        onChange={handleFullnameChange}
-                        disabled={!isNameEditable}
-                        ref={fullnameInputRef}
-                        onBlur={() => setIsNameEditable(false)}
-                        maxLength={20} // Add max length for character counting
+        <div className='flex flex-col items-center min-w-[25vw] max-w-[25vw] text-4xl p-8 bg-off-blue-200 text-dark-blue-900 rounded-4xl'>
+            <form className='flex flex-col items-center gap-8 min-w-[25vw] max-w-[25vw] text-4xl p-8 bg-off-blue-200 text-dark-blue-900 rounded-4xl' onSubmit={handleSubmit(onSubmit)}>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <img
+                        src="https://picsum.photos/2000.webp"
+                        alt="DP"
+                        className='w-60 rounded-[100%] border-8 border-dark-blue-900'
                     />
-                    {isNameEditable ? (
-                        <div className='absolute bottom-2 -right-6 text-sm text-mid-blue-700'>
-                            {(fullnameValue || '').length || 0}/20
-                        </div>
-                    ) : (
-                        <img
-                            src="/src/assets/edit-icon.svg"
-                            alt="Edit-Icon"
-                            className={`text-[12px] fill-dark-blue-900 absolute bottom-2 -right-4 cursor-pointer transition-all duration-300 ease-in-out ${nameEdit ? 'opacity-100 scale-100' : 'opacity-0 scale-90'}`}
-                            onClick={() => {
-                                setIsNameEditable(true)
-                                setTimeout(() => {
-                                    fullnameInputRef.current.focus()
-                                }, 0)
-                            }}
+                    <div className='flex justify-center items-baseline space-x-4 relative group'
+                        onMouseOver={() => { setNameEdit(true) }}
+                        onMouseOut={() => { setNameEdit(false) }}>
+                        <input
+                            type="text"
+                            value={fullnameValue}
+                            autoComplete='off'
+                            placeholder='Full Name'
+                            className='font-semibold text-center field-sizing-content focus:outline-none focus:border-b-4 focus:border-dark-blue-900 border-transparent bg-transparent text-dark-blue-900 placeholder-mid-blue-700'
+                            onChange={handleFullnameChange}
+                            disabled={!isNameEditable}
+                            ref={fullnameInputRef}
+                            onBlur={() => setIsNameEditable(false)}
+                            maxLength={20} // Add max length for character counting
                         />
-                    )}
+                        {isNameEditable ? (
+                            <div className='absolute bottom-2 -right-6 text-sm text-mid-blue-700'>
+                                {(fullnameValue || '').length || 0}/20
+                            </div>
+                        ) : (
+                            <img
+                                src="/src/assets/edit-icon.svg"
+                                alt="Edit-Icon"
+                                className={`text-[12px] fill-dark-blue-900 absolute bottom-2 -right-4 cursor-pointer transition-all duration-300 ease-in-out ${nameEdit ? 'opacity-100 scale-100' : 'opacity-0 scale-90'}`}
+                                onClick={() => {
+                                    setIsNameEditable(true)
+                                    setTimeout(() => {
+                                        fullnameInputRef.current.focus()
+                                    }, 0)
+                                }}
+                            />
+                        )}
+                    </div>
+                    <p className='text-2xl text-mid-blue-700'>@{props.username}</p>
                 </div>
-                <p className='text-2xl text-mid-blue-700'>@{props.username}</p>
-            </div>
 
-            <div className='flex flex-col justify-center items-center space-y-2'>
-                <div className='flex justify-center items-baseline space-x-4 relative group'
-                    onMouseOver={() => { setBioEdit(true) }}
-                    onMouseOut={() => { setBioEdit(false) }} >
-                    <textarea
-                        value={bioValue}
-                        autoComplete='off'
-                        placeholder="Bio"
-                        className='text-2xl font-semibold text-center field-sizing-content focus:outline-none focus:border-b-4 focus:border-dark-blue-900 border-transparent bg-transparent text-dark-blue-900 placeholder:text-3xl placeholder-mid-blue-700 w-full max-w-full resize-none overflow-y-hidden leading-tight wrap-break-words'
-                        onChange={handleBioChange}
-                        disabled={!isBioEditable}
-                        ref={bioInputRef}
-                        onBlur={() => setIsBioEditable(false)}
-                        rows={3}
-                        style={{
-                            wordWrap: 'break-word',
-                            overflowWrap: 'break-word',
-                            wordBreak: 'break-word',
-                            hyphens: 'auto'
-                        }}
-                        maxLength={75} // Add max length for character counting
-                    />
-                    {isBioEditable ? (
-                        <div className='absolute bottom-1 -right-6 text-sm text-mid-blue-700'>
-                            {(bioValue || '').length || 0}/75
-                        </div>
-                    ) : (
-                        <img
-                            src="/src/assets/edit-icon.svg"
-                            alt="Edit-Icon"
-                            className={`text-[12px] fill-dark-blue-900 absolute bottom-1 -right-4 cursor-pointer transition-all duration-300 ease-in-out ${bioEdit ? 'opacity-100 scale-100' : 'opacity-0 scale-90'}`}
-                            onClick={() => {
-                                setIsBioEditable(true)
-                                setTimeout(() => {
-                                    bioInputRef.current.focus()
-                                }, 0)
+                <div className='flex flex-col justify-center items-center space-y-2'>
+                    <div className='flex justify-center items-baseline space-x-4 relative group'
+                        onMouseOver={() => { setBioEdit(true) }}
+                        onMouseOut={() => { setBioEdit(false) }} >
+                        <textarea
+                            value={bioValue}
+                            autoComplete='off'
+                            placeholder="Bio"
+                            className='text-2xl font-semibold text-center field-sizing-content focus:outline-none focus:border-b-4 focus:border-dark-blue-900 border-transparent bg-transparent text-dark-blue-900 placeholder:text-3xl placeholder-mid-blue-700 w-full max-w-full resize-none overflow-y-hidden leading-tight wrap-break-words'
+                            onChange={handleBioChange}
+                            disabled={!isBioEditable}
+                            ref={bioInputRef}
+                            onBlur={() => setIsBioEditable(false)}
+                            rows={3}
+                            style={{
+                                wordWrap: 'break-word',
+                                overflowWrap: 'break-word',
+                                wordBreak: 'break-word',
+                                hyphens: 'auto'
                             }}
+                            maxLength={75} // Add max length for character counting
                         />
-                    )}
-                </div>
-                <div className='flex justify-center items-baseline space-x-4 relative group'
-                    onMouseOver={() => { setEmailEdit(true) }}
-                    onMouseOut={() => { setEmailEdit(false) }}>
-                    <input
-                        {...register("email")}
-                        type='text'
-                        value={emailValue}
-                        autoComplete='off'
-                        placeholder='Email Address'
-                        className='text-2xl text-center field-sizing-content focus:outline-none focus:border-b-4 focus:border-dark-blue-900 border-transparent bg-transparent text-dark-blue-900 placeholder-mid-blue-700'
-                        onChange={handleEmailChange}
-                        disabled={!isEmailEditable}
-                        ref={emailInputRef}
-                        onBlur={() => setIsEmailEditable(false)}
-                        maxLength={30} // Add max length for character counting
-                    />
-                    {isEmailEditable ? (
-                        <div className='absolute bottom-0.5 -right-6 text-sm text-mid-blue-700'>
-                            {(emailValue || '').length || 0}/30
-                        </div>
-                    ) : (
-                        <img
-                            src="/src/assets/edit-icon.svg"
-                            alt="Edit-Icon"
-                            className={`text-[12px] fill-dark-blue-900 absolute bottom-0.5 -right-4 cursor-pointer transition-all duration-300 ease-in-out ${emailEdit ? 'opacity-100 scale-100' : 'opacity-0 scale-90'}`}
-                            onClick={() => {
-                                setIsEmailEditable(true)
-                                setTimeout(() => {
-                                    emailInputRef.current.focus()
-                                }, 0)
-                            }}
+                        {isBioEditable ? (
+                            <div className='absolute bottom-1 -right-6 text-sm text-mid-blue-700'>
+                                {(bioValue || '').length || 0}/75
+                            </div>
+                        ) : (
+                            <img
+                                src="/src/assets/edit-icon.svg"
+                                alt="Edit-Icon"
+                                className={`text-[12px] fill-dark-blue-900 absolute bottom-1 -right-4 cursor-pointer transition-all duration-300 ease-in-out ${bioEdit ? 'opacity-100 scale-100' : 'opacity-0 scale-90'}`}
+                                onClick={() => {
+                                    setIsBioEditable(true)
+                                    setTimeout(() => {
+                                        bioInputRef.current.focus()
+                                    }, 0)
+                                }}
+                            />
+                        )}
+                    </div>
+                    <div className='flex justify-center items-baseline space-x-4 relative group'
+                        onMouseOver={() => { setEmailEdit(true) }}
+                        onMouseOut={() => { setEmailEdit(false) }}>
+                        <input
+                            {...register("email")}
+                            type='text'
+                            value={emailValue}
+                            autoComplete='off'
+                            placeholder='Email Address'
+                            className='text-2xl text-center field-sizing-content focus:outline-none focus:border-b-4 focus:border-dark-blue-900 border-transparent bg-transparent text-dark-blue-900 placeholder-mid-blue-700'
+                            onChange={handleEmailChange}
+                            disabled={!isEmailEditable}
+                            ref={emailInputRef}
+                            onBlur={() => setIsEmailEditable(false)}
+                            maxLength={30} // Add max length for character counting
                         />
-                    )}
+                        {isEmailEditable ? (
+                            <div className='absolute bottom-0.5 -right-6 text-sm text-mid-blue-700'>
+                                {(emailValue || '').length || 0}/30
+                            </div>
+                        ) : (
+                            <img
+                                src="/src/assets/edit-icon.svg"
+                                alt="Edit-Icon"
+                                className={`text-[12px] fill-dark-blue-900 absolute bottom-0.5 -right-4 cursor-pointer transition-all duration-300 ease-in-out ${emailEdit ? 'opacity-100 scale-100' : 'opacity-0 scale-90'}`}
+                                onClick={() => {
+                                    setIsEmailEditable(true)
+                                    setTimeout(() => {
+                                        emailInputRef.current.focus()
+                                    }, 0)
+                                }}
+                            />
+                        )}
+                    </div>
                 </div>
-            </div>
 
-            <div className='flex gap-16'>
-                <div className='flex flex-col justify-center items-center'>
-                    <p className='font-bold'>0</p>
-                    <p className='font-semibold text-2xl text-mid-blue-700'>Followers</p>
+                <div className='flex gap-16'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <p className='font-bold'>0</p>
+                        <p className='font-semibold text-2xl text-mid-blue-700'>Followers</p>
+                    </div>
+
+                    <div className='flex flex-col justify-center items-center'>
+                        <p className='font-bold'>0</p>
+                        <p className='font-semibold text-2xl text-mid-blue-700'>Following</p>
+                    </div>
                 </div>
 
-                <div className='flex flex-col justify-center items-center'>
-                    <p className='font-bold'>0</p>
-                    <p className='font-semibold text-2xl text-mid-blue-700'>Following</p>
-                </div>
-            </div>
-
-            {(isDirty && showUpdateBtn) && <input
-                type='submit'
-                value={isSubmitting ? "Processing..." : "Update"}
-                disabled={isSubmitting}
-                className='text-3xl bg-dark-blue-900 text-off-blue-200 font-semibold rounded my-4 px-8 py-4 hover:bg-mid-blue-700 cursor-pointer'
-            />}
-            {/* {isDirty && <div>
-                <button className='text-3xl bg-dark-blue-900 text-off-blue-200 font-semibold rounded my-4 px-8 py-4 hover:bg-mid-blue-700 cursor-pointer'>
+                {(isDirty && showUpdateBtn) && <input
+                    type='submit'
+                    value={isSubmitting ? "Processing..." : "Update"}
+                    disabled={isSubmitting}
+                    className='text-3xl bg-dark-blue-900 text-off-blue-200 font-semibold rounded my-4 px-8 py-4 hover:bg-mid-blue-700 cursor-pointer disabled:bg-light-blue-500 disabled:cursor-not-allowed'
+                />}
+            </form>
+            {!isDirty && <div>
+                <a href="http://localhost:3000/logout"><button className='text-3xl bg-dark-blue-900 text-off-blue-200 font-semibold rounded my-4 px-8 py-4 hover:bg-mid-blue-700 cursor-pointer'>
                     Logout
-                </button>
-            </div>} */}
-        </form>
+                </button></a>
+            </div>}
+        </div>
     )
 }
 
