3454280b53e5340e058c6edcc4cfd1e6ffe2fa93
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-11-29 04:18:00 +0500

    Refactor Post component to enhance like/dislike button layout and display counters

--- src/components/subComponents/Post.jsx
+++ src/components/subComponents/Post.jsx
@@ -15,30 +15,29 @@ const Post = (props) => {
                 </div>
             </div>
             <div>Content of the Post</div>
-            <div className='flex gap-2 items-center text-sm border-t-2 border-off-blue-200 py-2 font-semibold'>
-                <span>Likes Counter</span>
-                <span>|</span>
-                <span>Dislikes Counter</span>
-            </div>
-            <div className='flex justify-around gap-2'>
-                <button className='flex gap-2 justify-center py-2 min-w-[49%] cursor-pointer rounded-4xl font-semibold border-2 border-off-blue-200 text-off-blue-200 hover:bg-off-blue-200 hover:text-dark-blue-900 group'>
-                    <img
-                        src="/src/assets/like-icon.svg"
-                        alt="like-button"
-                        className='w-8 filter filter-[invert(88%)_sepia(5%)_saturate(2050%)_hue-rotate(166deg)_brightness(100%)_contrast(106%)] group-hover:filter-[invert(12%)_sepia(65%)_saturate(1494%)_hue-rotate(200deg)_brightness(91%)_contrast(95%)]'
-                    />
-                    <span>Like</span>
-                </button>
-
-                <button className='flex gap-2 justify-center py-2 min-w-[49%] cursor-pointer rounded-4xl font-semibold border-2 border-off-blue-200 text-off-blue-200 hover:bg-off-blue-200 hover:text-dark-blue-900 group'>
-                    <img
-                        src="/src/assets/dislike-icon.svg"
-                        alt="dislike-button"
-                        className='w-8 filter filter-[invert(88%)_sepia(5%)_saturate(2050%)_hue-rotate(166deg)_brightness(100%)_contrast(106%)] group-hover:filter-[invert(12%)_sepia(65%)_saturate(1494%)_hue-rotate(200deg)_brightness(91%)_contrast(95%)]'
-                    />
-                    <span>Dislike</span>
-                </button>
-
+            <div className='flex justify-around gap-2 font-semibold border-t-2 border-off-blue-200 pt-2'>
+                <div className='flex flex-col justify-center items-center gap-2 min-w-[49%]'>
+                    <span className='text-lg'>0 Likes</span>
+                    <button className='flex gap-2 justify-center py-2 min-w-full cursor-pointer rounded-4xl border-2 border-off-blue-200 text-off-blue-200 hover:bg-off-blue-200 hover:text-dark-blue-900 group'>
+                        <img
+                            src="/src/assets/like-icon.svg"
+                            alt="like-button"
+                            className='w-8 filter filter-[invert(88%)_sepia(5%)_saturate(2050%)_hue-rotate(166deg)_brightness(100%)_contrast(106%)] group-hover:filter-[invert(12%)_sepia(65%)_saturate(1494%)_hue-rotate(200deg)_brightness(91%)_contrast(95%)]'
+                        />
+                        <span>Like</span>
+                    </button>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2 min-w-[49%]'>
+                    <span className='text-lg'>0 Dislikes</span>
+                    <button className='flex gap-2 justify-center py-2 cursor-pointer min-w-full rounded-4xl border-2 border-off-blue-200 text-off-blue-200 hover:bg-off-blue-200 hover:text-dark-blue-900 group'>
+                        <img
+                            src="/src/assets/dislike-icon.svg"
+                            alt="dislike-button"
+                            className='w-8 filter filter-[invert(88%)_sepia(5%)_saturate(2050%)_hue-rotate(166deg)_brightness(100%)_contrast(106%)] group-hover:filter-[invert(12%)_sepia(65%)_saturate(1494%)_hue-rotate(200deg)_brightness(91%)_contrast(95%)]'
+                        />
+                        <span>Dislike</span>
+                    </button>
+                </div>
             </div>
         </div>
     )
