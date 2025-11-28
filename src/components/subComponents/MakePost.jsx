8a3c867d411d9bb924624f1211984fd0c6710d57
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-11-29 01:59:00 +0500

    Import useContext in MakePost component to make app run without errors

--- src/components/subComponents/MakePost.jsx
+++ src/components/subComponents/MakePost.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form'
 
 const MakePost = (props) => {
