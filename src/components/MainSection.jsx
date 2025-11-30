58d1b6ae2dfdfd833ad3b07cfebd8007457e2674
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-11-30 05:43:09 +0500

    Refactor MainSection component to sort posts by creation date before rendering

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -28,21 +28,23 @@ const MainSection = () => {
         <div className='min-w-[44%] max-w-[44%] flex flex-col gap-8 p-8 bg-off-blue-200 text-dark-blue-900 rounded-4xl max-h-screen'>
             <MakePost />
             <div className='space-y-4 flex-1 overflow-y-auto [&::-webkit-scrollbar]:w-0'>
-                {posts.map((post) => {
-                    return (<Post
-                        key={post._id}
-                        fullname={post.author.fullname}
-                        // username={post.author.username}
-                        content={post.content}
-                        likes={post.likes.length}
-                        dislikes={post.dislikes.length}
-                        createdAt={new Date(post.createdAt).toLocaleDateString("en-GB", {
-                            day: "numeric",
-                            month: "long",
-                            year: "numeric",
-                        })}
-                    />)
-                })}
+                {[...posts].sort((o, n) => {
+                    return (new Date(n.createdAt) - new Date(o.createdAt))
+                }).map((post) => {
+                        return (<Post
+                            key={post._id}
+                            fullname={post.author.fullname}
+                            // username={post.author.username}
+                            content={post.content}
+                            likes={post.likes.length}
+                            dislikes={post.dislikes.length}
+                            createdAt={new Date(post.createdAt).toLocaleDateString("en-GB", {
+                                day: "numeric",
+                                month: "long",
+                                year: "numeric",
+                            })}
+                        />)
+                    })}
             </div>
         </div>
 
