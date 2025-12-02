703c51194dc8581390815333f0f61fecbaadb9c0
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-12-02 05:28:09 +0500

    Implement the functionality only one button remain active from followed or not followed buttons

--- src/components/subComponents/FollowedAndUnfollowedNavbar.jsx
+++ src/components/subComponents/FollowedAndUnfollowedNavbar.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FollowedAndUnfollowedNavbar = (props) => {
+    const [active, setActive] = useState("notfollowed")
 
     return (
         <div className="bg-off-blue-200 flex justify-around gap-2 font-semibold p-4 sticky top-0 z-10">
             <button
-                className='py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900 text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200 group min-w-[49%]'
+                className={`py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900 text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200 group min-w-[49%] ${active === "followed" ? "bg-dark-blue-900 text-off-blue-200" : "text-dark-blue-900"}`}
                 onClick={() => {
                     props.setCategory("followed")
-                    alert("followed")
-                }} >
+                    setActive("followed")
+                }}
+            >
                 Followed
             </button>
             <button
-                className='py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900 text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200 group min-w-[49%]'
+                className={`py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900 text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200 group min-w-[49%] ${active === "notFollowed" ? "bg-dark-blue-900 text-off-blue-200" : "text-dark-blue-900"}`}
                 onClick={() => {
                     props.setCategory("notFollowed")
-                    alert("not followed clicked")
+                    setActive("notFollowed")
                 }} >
                 Not Followed
             </button>
-        </div>
+        </div >
     )
 }
 
