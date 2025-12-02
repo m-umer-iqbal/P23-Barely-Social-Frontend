9f51fd909d775a35040a0cc7f45d735dc0a0367b
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-12-03 04:01:15 +0500

    Fix the Followed and Unfollowed Navbar design

--- src/components/subComponents/FollowedAndUnfollowedNavbar.jsx
+++ src/components/subComponents/FollowedAndUnfollowedNavbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
 const FollowedAndUnfollowedNavbar = (props) => {
-    const [active, setActive] = useState("notfollowed")
+    const [active, setActive] = useState("followed")
 
     return (
         <div className="bg-off-blue-200 flex justify-around gap-2 font-semibold p-4 sticky top-0 z-10">
             <button
-                className={`py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900 text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200 group min-w-[49%] ${active === "followed" ? "bg-dark-blue-900 text-off-blue-200" : "text-dark-blue-900"}`}
+                className={`py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900  group min-w-[49%] ${active === "followed" ? "bg-dark-blue-900 text-off-blue-200" : "text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200"}`}
                 onClick={() => {
                     props.setCategory("followed")
                     setActive("followed")
@@ -15,7 +15,7 @@ const FollowedAndUnfollowedNavbar = (props) => {
                 Followed
             </button>
             <button
-                className={`py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900 text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200 group min-w-[49%] ${active === "notFollowed" ? "bg-dark-blue-900 text-off-blue-200" : "text-dark-blue-900"}`}
+                className={`py-2 cursor-pointer rounded-4xl border-2 border-dark-blue-900  group min-w-[49%] ${active === "notFollowed" ? "bg-dark-blue-900 text-off-blue-200" : "text-dark-blue-900 hover:bg-dark-blue-900 hover:text-off-blue-200"}`}
                 onClick={() => {
                     props.setCategory("notFollowed")
                     setActive("notFollowed")
